068da92fb7e9017f83e145fd165513c557da2fb0
Devwon-1999 <Devwon.1999@gmail.com>
2024-08-05 20:56:54 +0900

    1244

---
+++ 1244.2day-maxprizemoney.py
@@ -0,0 +1,12 @@
+# SWEA D3
+# 1244. [S/W 문제해결 응용] 2일차 - 최대 상금
+# https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=2&problemLevel=3&contestProbId=AV15Khn6AN0CFAYD&categoryId=AV15Khn6AN0CFAYD&categoryType=CODE&problemTitle=&orderBy=SUBMIT_COUNT&selectCodeLang=ALL&select-1=3&pageSize=10&pageIndex=1
+
+T = int(input())
+
+for test_case in range(1, T + 1):
+    #입력
+    value, count = map(int, input().split())
+    str_value = str(value)
+
+    #메인 알고리즘(미완)
