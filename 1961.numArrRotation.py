8d0a1647928d4f4f3083cd59d67d52961e69d137
Devwon-1999 <Devwon.1999@gmail.com>
2024-08-07 23:00:44 +0900

    1961 main algorithm

--- 1961.numArrRotation.py
+++ 1961.numArrRotation.py
@@ -4,6 +4,14 @@
 
 T = int(input())
 
+#메인 알고리즘
+def rotation(arr):
+    arrReturn = [[0] * N for _ in range(N)]
+    for i in range(N):
+        for j in range(N):
+            arrReturn[i][j] = arr[N-1-j][i]
+    return arrReturn
+
 for test_case in range(1, T + 1):
     #입력
     N = int(input())
@@ -11,11 +19,11 @@ for test_case in range(1, T + 1):
 
     #90, 180, 270 뒤집음
     numList90 = rotation(numList)
-    numList180 = rotation(numList180)
-    numList270 = rotation(numList270)
+    numList180 = rotation(numList90)
+    numList270 = rotation(numList180)
 
 
     #출력
     print(f"#{test_case}")
     for a, b, c in zip(numList90, numList180, numList270):
-        print(f"{a} {b} {c}")
+        print(f"{''.join(map(str, a))} {''.join(map(str, b))} {''.join(map(str, c))}")
